4e502370c64901afd6e4524fc6b9ed6fedfa08be
aarondickey <aarondickey2@gmail.com>
2020-02-18 18:36:45 -0500

    Changed index.cshtm to reflect site purpose

--- ip/Controllers/HomeController.cs
+++ ip/Controllers/HomeController.cs
@@ -20,6 +20,8 @@ namespace ip.Controllers
 
         public IActionResult Index()
         {
+            string ipAddress = HttpContext.Connection.RemoteIpAddress.ToString();
+            ViewBag.IPAddress = ipAddress;
             return View();
         }
 
